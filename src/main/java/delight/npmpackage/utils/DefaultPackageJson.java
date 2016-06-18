00bd18d78e351c1ef5d27897b2d27645b33d1e41
mxro <mxro@nowhere.com>
2016-06-18 12:18:46 +1000

    Develop Node.js integration Autocommit Sat Jun 18 12:18:46 AEST 2016

---
+++ src/main/java/delight/npmpackage/utils/DefaultPackageJson.java
@@ -0,0 +1,10 @@
+package delight.npmpackage.utils;
+
+public class DefaultPackageJson {
+
+    public static String get() {
+        return "{\n" + "    \"name\": \"service\",\n" + "    \"version\": \"0.0.0\",\n" + "    \"description\": \"\",\n"
+                + "    \"main\": \"index.js\"\n" + "}";
+    }
+
+}
---
+++ src/test/java/delight/npmpackage/tests/ManualTest.java
@@ -0,0 +1,23 @@
+package delight.npmpackage.tests;
+
+import delight.npmpackage.utils.DefaultPackageJson;
+
+import java.io.File;
+import java.io.IOException;
+
+import de.mxro.file.Jre.FilesJre;
+import de.mxro.process.Spawn;
+
+public class ManualTest {
+
+    public static void main(final String[] args) throws IOException {
+
+        final File workDir = new File("/home/adminuser/Desktop/tmp");// Files.createTempDirectory("npmwork").toFile();
+
+        FilesJre.wrap(workDir).assertFile("package.json").setText(DefaultPackageJson.get());
+
+        System.out.println(Spawn.sh(workDir, "npm install async --save"));
+
+    }
+
+}
