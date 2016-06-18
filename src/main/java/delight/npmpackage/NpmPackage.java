56f376b52f0425be6c278f9ffd7ea4ec06a46eb6
mxro <mxro@nowhere.com>
2016-06-18 12:23:46 +1000

    Develop Node.js integration Autocommit Sat Jun 18 12:23:46 AEST 2016

--- src/main/java/delight/npmpackage/NpmPackage.java
+++ src/main/java/delight/npmpackage/NpmPackage.java
@@ -1,8 +1,11 @@
 package delight.npmpackage;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 import de.mxro.process.Spawn;
@@ -24,6 +27,16 @@ public class NpmPackage {
         } catch (final IOException e) {
             throw new RuntimeException(e);
         }
+
+    }
+
+    private final static String getPackageJson(final File zipFile) {
+
+        final byte[] buffer = new byte[2048];
+
+        // open the zip file stream
+        final InputStream fis = new FileInputStream(zipFile);
+        final ZipInputStream zis = new ZipInputStream(fis);
 
     }
 
