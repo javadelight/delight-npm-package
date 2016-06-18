a1a2cb8349d115348caca106808e56089f63b9f3
mxro <mxro@nowhere.com>
2016-06-18 11:57:46 +1000

    Develop Node.js integration Autocommit Sat Jun 18 11:57:46 AEST 2016

--- src/main/java/delight/npmpackage/NpmPackage.java
+++ src/main/java/delight/npmpackage/NpmPackage.java
@@ -1,5 +1,19 @@
 package delight.npmpackage;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.zip.ZipOutputStream;
+
 public class NpmPackage {
+
+    public static void perform(final ZipOutputStream target, final String[] npmDependencies) {
+
+        try {
+            final File workDir = File.createTempFile("temp-file-name", ".tmp");
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 }
