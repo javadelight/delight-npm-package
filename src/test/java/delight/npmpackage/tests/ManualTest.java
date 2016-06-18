50da0473288b3c8225e2a8a0d7780aba805f5309
mxro <mxro@nowhere.com>
2016-06-18 12:42:46 +1000

    Develop Node.js integration Autocommit Sat Jun 18 12:42:46 AEST 2016

--- src/test/java/delight/npmpackage/tests/ManualTest.java
+++ src/test/java/delight/npmpackage/tests/ManualTest.java
@@ -10,7 +10,7 @@ public class ManualTest {
 
     public static void main(final String[] args) throws IOException {
 
-        final File target = new File("/home/adminuser/Desktop/tmp/service.zip");// Files.createTempDirectory("npmwork").toFile();
+        final File target = new File("/home/adminuser/Desktop/dest/service.zip");// Files.createTempDirectory("npmwork").toFile();
 
         NpmPackage.perform("var async = require('async');", DefaultPackageJson.get(), target, new String[] { "async" });
 
