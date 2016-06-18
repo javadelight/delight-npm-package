4eef12a44920d0e2f7c34d7a946d7edd508caa1d
mxro <mxro@nowhere.com>
2016-06-18 12:29:46 +1000

    Develop Node.js integration Autocommit Sat Jun 18 12:29:46 AEST 2016

--- src/main/java/delight/npmpackage/NpmPackage.java
+++ src/main/java/delight/npmpackage/NpmPackage.java
@@ -1,7 +1,9 @@
 package delight.npmpackage;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
@@ -30,13 +32,23 @@ public class NpmPackage {
 
     }
 
-    private final static String getPackageJson(final ZipFile zipFile) {
-        final Enumeration entries = zipFile.entries();
+    private final static String getPackageJson(final ZipFile zipFile) throws IOException {
+        final Enumeration<? extends ZipEntry> entries = zipFile.entries();
 
         while (entries.hasMoreElements()) {
-            final ZipEntry ze = (ZipEntry) entries.nextElement();
+            final ZipEntry ze = entries.nextElement();
             if (ze.getName() == "package.json") {
 
+                final BufferedReader br = new BufferedReader(new InputStreamReader(zipFile.getInputStream(ze)));
+
+                String lines = "";
+                String line;
+                while ((line = br.readLine()) != null) {
+                    lines += line;
+                }
+
+                br.close();
+                return lines;
             }
         }
 
