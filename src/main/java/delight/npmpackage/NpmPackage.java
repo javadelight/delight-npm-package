82bde06e533a704d9838a755cd7efe68be2def2d
mxro <mxro@nowhere.com>
2016-06-18 12:06:46 +1000

    Develop Node.js integration Autocommit Sat Jun 18 12:06:46 AEST 2016

--- src/main/java/delight/npmpackage/NpmPackage.java
+++ src/main/java/delight/npmpackage/NpmPackage.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.zip.ZipOutputStream;
 
+import de.mxro.process.Spawn;
+
 public class NpmPackage {
 
     public static void perform(final ZipOutputStream target, final String[] npmDependencies) {
@@ -14,6 +16,10 @@ public class NpmPackage {
             // ".tmp");
 
             final File workDir = Files.createTempDirectory("npmwork").toFile();
+
+            for (final String dependency : npmDependencies) {
+                System.out.println(Spawn.sh("npm install " + dependency + " --save"));
+            }
 
         } catch (final IOException e) {
             throw new RuntimeException(e);
